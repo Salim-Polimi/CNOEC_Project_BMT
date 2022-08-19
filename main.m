f7f50c7a0748d7cf4764a0b2de2543d9da777708
Filippo Tallon <filippotallon@gmail.com>
2022-08-19 13:40:52 +0200

    added new heuristic and results folder as database for optimization results

--- main.m
+++ main.m
@@ -17,10 +17,8 @@ parameters.Voc      =   240;    % *1 should be variable wrt state of charge, see
 parameters.R0       =   0.13;   % *1 should be variable wrt state of charge, see pag.92
 parameters.Qnom     =   23400;  % *1
 parameters.eta_coul =   0.95;
-
 parameters.dim_decision_var = 4;
 parameters.interval_size = 1800/parameters.dim_decision_var; % (dimension of the decision variable) will be length of u_compressed
-
 parameters.initial_soc = 0.8;
 
 % read wltp excel file
@@ -30,9 +28,16 @@ parameters.v_vec = wltp_cycle(:,2)/3.6;
 parameters.a_vec = wltp_cycle(:,3);
 
 %% Plot speed and acceleration profile
-% figure(1),
-% subplot(2,1,1),plot(parameters.time_vec, parameters.v_vec*3.6),grid on, hold on,xlabel('time [s]'),ylabel('v_ref [km/h]')
-% subplot(2,1,2),plot(parameters.time_vec, parameters.a_vec),grid on, hold on,xlabel('time [s]'),ylabel('a_ref [m/s^2]')
+figure(1), set(gcf, 'Color', 'White'),
+sp(1) = subplot(2,1,1);
+hold on, grid on, box on
+plot(parameters.time_vec, parameters.v_vec*3.6, 'LineWidth',1.5,'Color','b')
+xlabel('Time [s]'), ylabel('Speed in the driving cycle [km/h]')
+sp(2) = subplot(2,1,2);
+hold on, grid on, box on
+plot(parameters.time_vec, parameters.a_vec, 'LineWidth',1.5,'Color','b')
+xlabel('Time [s]'), ylabel('Acceleration in the driving cycle [m/s^2')
+linkaxes(sp,'x'), clear sp
 
 %% Retrive efficiency map
 load lasso_param.mat
@@ -66,9 +71,7 @@ myoptions.ls_tkmax      =	1;
 myoptions.ls_beta       =	0.1;
 myoptions.ls_c          =	0.1;
 myoptions.ls_nitermax   =	10;
-
 myoptions.nitermax      =	5;
-
 myoptions.xsequence     =	'on';
 myoptions.GN_funF       =   @(x)big_fun(x,parameters);
 myoptions.GN_sigma      =   1e-3;
@@ -78,10 +81,17 @@ x0 = zeros(interval_size,1);
 % number of nonlinear constraints
 p = 1; % equaliti constrain defined as "g = soc_vec(N,1) - soc_vec(1,1);" in "big_fun.m" and "fun.m"
 q = 2*N; % N inequality constraint "soc_vec(ind,1) > 0.1 for every Ts" and N inequality constraint "soc_vec(ind,1) < 0.9 for every Ts"
-% Run solver
+
+%% Code generation
+% codegen big_fun -args {coder.typeof(x0),coder.typeof(parameters)}
+% codegen fun -args {coder.typeof(x0),coder.typeof(parameters)}
+% codegen fuel_consumption -args {coder.typeof(parameters),coder.typeof(tvec),coder.typeof(p)}
+
+%% Run optimization
 tic
 [xstar,fxstar,niter,exitflag,xsequence] = myfmincon(@(x)fun(x,parameters),x0,A,b,C,d,p,q,myoptions);
 toc
+
 %% xstar_decompressed computation:
 xstar_decompressed=[];
 for i=1:length(xstar)
@@ -90,6 +100,7 @@ for i=1:length(xstar)
     end
 end
 xstar_decompressed = [xstar_decompressed; xstar_decompressed(end)];
+
 %% Simulation (only ICE)
 m_f1 = zeros(N,1);        % this will be the integral in tvec of m_f_dot
 m_f_dot_vec = zeros(N,1);
@@ -115,32 +126,39 @@ for ind=2:N
     soc_vec1(ind,1) = soc_vec1(ind-1,1)+Ts*soc_dot;
 end
 
-%% Simulation ICE only + EM from regenerative braking
-% m_f2 = zeros(N,1);        % this will be the integral in tvec of m_f_dot
-% m_f_dot_vec = zeros(N,1);
-% soc_dot_vec = zeros(N,1);
-% soc_vec2 = zeros(N,1);
-% 
-% % initialization of the soc -> start from a safe segion SOC=0.9
-% soc_vec2(1,1) = parameters.initial_soc;
-% 
-% % second strategy -> only EM until we have battery, then switch to ICE  (uncomment and comment prevous line)
-% u_vec = zeros(N,1);
-% 
-% 
-% for ind=2:N
-%     [m_f_dot,soc_dot] = fuel_consumption(parameters, u_vec(ind,1),ind);
-%     
-%     m_f_dot_vec(ind,1) = m_f_dot; 
-%     m_f2(ind,1) = m_f2(ind-1,1)+Ts*m_f_dot; 
-% 
-%     soc_dot_vec(ind,1) = soc_dot;
-%     soc_vec2(ind,1) = soc_vec2(ind-1,1)+Ts*soc_dot;
-%     
-%     if soc_vec2(ind,1) <= 0.1
-%         u_vec(ind+1,1) = 0;
-%     end
-% end
+%% Simulation Heuristic
+m_f2 = zeros(N,1);        % this will be the integral in tvec of m_f_dot
+m_f_dot_vec = zeros(N,1);
+soc_dot_vec = zeros(N,1);
+soc_vec2 = zeros(N,1);
+
+% initialization of the soc -> start from a safe segion SOC=0.9
+soc_vec2(1,1) = parameters.initial_soc;
+
+% heuristic
+u_vec = zeros(N,1);
+
+for ind=2:N
+    if parameters.v_vec(ind)*3.6 < 30
+        u_vec(ind,1) = 1;
+    end
+    if parameters.v_vec(ind)*3.6 > 80
+        u_vec(ind,1) = -0.3;
+    end
+    
+    
+    [m_f_dot,soc_dot] = fuel_consumption(parameters, u_vec(ind,1),ind);
+    
+    m_f_dot_vec(ind,1) = m_f_dot; 
+    m_f2(ind,1) = m_f2(ind-1,1)+Ts*m_f_dot; 
+
+    soc_dot_vec(ind,1) = soc_dot;
+    soc_vec2(ind,1) = soc_vec2(ind-1,1)+Ts*soc_dot;
+    
+    if soc_vec2(ind,1) <= 0.1
+        u_vec(ind+1,1) = 0;
+    end
+end
 %% Simulation (optimum control)
 m_f3 = zeros(N,1); % this will be the integral in tvec of m_f_dot
 m_f_dot_vec = zeros(N,1);
@@ -175,24 +193,33 @@ for ind=2:N
 end
 
 %% Plot the results
-close all
+figure(2), set(gcf, 'Color', 'White'),
+grid on, hold on,xlabel('time [s]'),ylabel('fuel consumption [kg]')
+plot(tvec, m_f1,'LineWidth',1.5,'color', 'b', 'DisplayName', 'ICE Only')
+plot(tvec, m_f2,'LineWidth',1.5,'color', 'r', 'DisplayName', 'Heuristic')
+plot(tvec, m_f3,'LineWidth',1.5,'color', 'g', 'DisplayName', 'Optimum Ctrl')
+legend show
 
-figure(1),
-plot(tvec, m_f1,'LineWidth',1.5),grid on, hold on,xlabel('time [s]'),ylabel('fuel consumption [kg]')
-plot(tvec, m_f3,'LineWidth',1.5,'color', 'g'),grid on, hold on,xlabel('time [s]'),ylabel('fuel consumption [kg]')
-legend('ICE only', 'Optimum Ctrl')
+figure(3), set(gcf, 'Color', 'White'),
+grid on, hold on, xlabel('time [s]'),ylabel('soc'), axis([-inf inf 0 1])
+plot(tvec, soc_vec1,'LineWidth',1.5,'color', 'b', 'DisplayName', 'ICE Only')
+plot(tvec, soc_vec2,'LineWidth',1.5,'color', 'r', 'DisplayName', 'Heuristic')
+plot(tvec, soc_vec3,'LineWidth',1.5,'color', 'g', 'DisplayName', 'Optimum Ctrl')
+legend show
 
-figure(2),
-plot(tvec, soc_vec1,'LineWidth',1.5),grid on, hold on, xlabel('time [s]'),ylabel('soc')
-plot(tvec, soc_vec3,'LineWidth',1.5,'color', 'g'),grid on, hold on, xlabel('time [s]'),ylabel('soc')
-legend('ICE only', 'Optimum Ctrl')
-axis([-inf inf 0 1])
-
-figure(3)
-plot(tvec, xstar_decompressed,'LineWidth',1.5),grid on, hold on, xlabel('time [s]'),ylabel('control variable')
-legend('Optimum Ctrl')
-axis([-inf inf -1.2 1.2])
-
-
+figure(4), set(gcf, 'Color', 'White'),
+sp(1) = subplot(3,1,1);
+hold on, grid on, box on
+plot(parameters.time_vec, parameters.v_vec*3.6, 'LineWidth',1.5,'Color','b')
+xlabel('Time [s]'), ylabel('Speed in the driving cycle [km/h]')
+sp(2) = subplot(3,1,2);
+hold on, grid on, box on
+plot(parameters.time_vec, parameters.a_vec, 'LineWidth',1.5,'Color','b')
+xlabel('Time [s]'), ylabel('Acceleration in the driving cycle [m/s^2]')
+sp(3) = subplot(3,1,3);
+hold on, grid on, box on
+plot(tvec, xstar_decompressed,'LineWidth',1.5, 'LineWidth',1.5,'Color','b')
+xlabel('Time [s]'), ylabel('Control variable u'), axis([-inf inf -1.2 1.2])
+linkaxes(sp,'x'), clear sp
 
 
